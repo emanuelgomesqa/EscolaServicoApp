12ae5fdf950dfc8d92afb2e44f3890b9265b0c86
Emanuel Gomes <emanuel.gomes@academico.ifpb.edu.br>
2019-07-22 10:22:08 -0300

    Update Escola.py

--- Entidades/Escola.py
+++ Entidades/Escola.py
@@ -1,5 +1,6 @@
 class Escola ():
-    def __init__(self, nome, logradouro, cidade):
+    def __init__(self,id_escola, nome, logradouro, cidade):
+        self.id_escola = id_escola
         self.nome = nome
         self.logradouro = logradouro
         self.cidade = cidade
